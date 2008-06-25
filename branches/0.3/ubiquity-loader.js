9d7a86329cb3c78a7feecf3c700f10d4efce9484
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-06-25 17:24:05 +0000

    Added loader-end, and renamed loader.js to loader-begin.js Modified Ubiquity loader to work in IE.

--- branches/0.3/ubiquity-loader.js
+++ branches/0.3/ubiquity-loader.js
@@ -1,22 +1,20 @@
 arrScripts = [
+	  "http://yui.yahooapis.com/2.5.2/build/yuiloader/yuiloader-beta-min.js",
 	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/ie-instance-fixer.js",
 	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/set-document-loaded.js",
-    "http://yui.yahooapis.com/2.5.2/build/yuiloader/yuiloader-beta-min.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/loader.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/main.js"
+	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/loader-begin.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/main.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/loader-end.js"
 ];
 
-var arrScriptElements = new Array();
-
-for (i in arrScripts) {
+var arrScriptElements = [];
+var l = arrScripts.length;
+//for (var i = l-1;i >=0;--i) {
+arrScriptElements.push("<script>alert('loading')</script>");
+for (var i = 0 ; i < l ; ++i) {
   arrScriptElements.push('<script src="' + arrScripts[i] +'" type="text/javascript">/**/</script>');
 }
 
-arrScriptElements.push(
-  '<script type="text/javascript"> \
-    window.status = "Loading Ubiquity modules..."; \
-    loader.insert(); \
-  </script>'
-);
-
 document.write(arrScriptElements.join("\n"));
+
+
