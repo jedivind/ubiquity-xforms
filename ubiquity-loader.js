b13ee4fa09534aa1dc741566fe8d92a1f68e2e6c
markbirbeck <markbirbeck@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-06-26 10:36:49 +0000

    Updated paths to modules to use 'tags/0.3.0'.

--- ubiquity-loader.js
+++ ubiquity-loader.js
@@ -1,19 +1,17 @@
 arrScripts = [
 	  "http://yui.yahooapis.com/2.5.2/build/yuiloader/yuiloader-beta-min.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/ie-instance-fixer.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/set-document-loaded.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/loader-begin.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/main.js",
-	  "http://ubiquity-xforms.googlecode.com/svn/branches/0.3/lib/xforms/loader-end.js"
+	  "http://ubiquity-xforms.googlecode.com/svn/tags/0.3.0/lib/xforms/ie-instance-fixer.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/tags/0.3.0/lib/xforms/set-document-loaded.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/tags/0.3.0/lib/xforms/loader-begin.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/tags/0.3.0/lib/xforms/main.js",
+	  "http://ubiquity-xforms.googlecode.com/svn/tags/0.3.0/lib/xforms/loader-end.js"
 ];
 
 var arrScriptElements = [];
 var l = arrScripts.length;
-//for (var i = l-1;i >=0;--i) {
+
 for (var i = 0 ; i < l ; ++i) {
   arrScriptElements.push('<script src="' + arrScripts[i] +'" type="text/javascript">/**/</script>');
 }
 
 document.write(arrScriptElements.join("\n"));
-
-
