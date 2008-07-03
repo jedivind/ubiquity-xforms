8b10ba3bb75575f966780dfadc5dbda4b72e0829
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-07-03 13:38:01 +0000

    Created branch 're-actions' in order to implement the re.... actions.

---
+++ branches/re-actions/ubiquity-loader.js
@@ -0,0 +1,63 @@
+function pathToModule(module) { 
+  if (!module) {
+    throw("Missing or null parameter supplied.");
+  }
+  var childNodes = document.childNodes; 
+  var l = childNodes.length;
+  var i;
+  var head;
+  for (i = 0; i < l; ++i) {
+    if(childNodes[i].nodeName.toLowerCase() === "html") {
+      head = childNodes[i].firstChild;
+    }
+  }
+  
+  childNodes = head.childNodes;
+  l = childNodes.length;
+  var s = null;
+  var el, src, pos;
+
+  for (i = 0; i < l; ++i) {
+    el = childNodes[i];
+    if (el.nodeType === 1 && el.nodeName === "SCRIPT") {
+      src = el.src;
+      if (src) {
+        pos = src.lastIndexOf(module + ".js");
+        
+        if (pos != -1 && (!pos || src.charAt(pos - 1) === "/" || src.charAt(pos - 1) === "\\")) {
+          s = src.slice(0, pos);
+          break;
+        }
+      }// if @src is present
+    }// if we have a script element
+  }// for each child node
+
+  if (!s) {
+    throw("No Module called '" + module + "' was found.");
+  }
+  return s;
+}
+
+var baseDefaultPath = pathToModule("ubiquity-loader");
+g_sBehaviourDirectory = baseDefaultPath + "behaviours/";
+
+(
+  function()
+  {
+    var arrScripts = [
+      "http://yui.yahooapis.com/2.5.2/build/yuiloader/yuiloader-beta-min.js",
+      baseDefaultPath + "lib/xforms/ie-instance-fixer.js",
+      baseDefaultPath + "lib/xforms/set-document-loaded.js",
+      baseDefaultPath + "lib/xforms/loader-begin.js",
+      baseDefaultPath + "lib/xforms/xforms-loader.js",
+      baseDefaultPath + "lib/xforms/loader-end.js"
+    ];
+    var arrScriptElements = [ ];
+    var i, l = arrScripts.length;
+
+    for (i = 0 ; i < l ; ++i) {
+      arrScriptElements.push('<script src="' + arrScripts[i] +'" type="text/javascript">/**/</script>');
+    }
+    document.write(arrScriptElements.join("\n"));
+  }()
+);
