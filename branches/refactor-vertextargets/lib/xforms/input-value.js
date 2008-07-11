9e50ecbf43015d38a2ec049dc68c1d7d4b100bbb
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-07-11 09:31:45 +0000

    Created branch 'refactor-vertextargets, in order to write appropriate tests for vertextargets.js and reduce its impact on the global namespace.

---
+++ branches/refactor-vertextargets/lib/xforms/input-value.js
@@ -0,0 +1,135 @@
+/*
+ * Copyright (C) 2008 Backplane Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function XFormsInputValue(elmnt)
+{
+	element = elmnt,
+	currValue = "",
+	m_bFirstSetValue = true
+}
+
+function valueChangedIE(pThis,evt)
+{
+	/*
+	 * [ISSUE] Not really suitable to use mutation events.
+	 */
+
+	var oEvt = pThis.element.ownerDocument.createEvent("MutationEvents");
+	
+	oEvt.initEvent("control-value-changed", true, true,
+		null, pThis.currValue, evt.srcElement.value, null, null);
+
+	spawn(function(){FormsProcessor.dispatchEvent(pThis.element,oEvt);});
+
+	/*
+	 * Cancel bubbling but don't cancel the event itself
+	 * otherwise we never get the value actually changed.
+	 */
+
+	 evt.cancelBubble = true;
+
+}
+
+function valueChangedFF(pThis,evt)
+{
+	/*
+	 * [ISSUE] Not really suitable to use mutation events.
+	 */
+	var oEvt = pThis.element.ownerDocument.createEvent("MutationEvents");
+	
+	oEvt.initMutationEvent("control-value-changed", true, true,
+		null, pThis.currValue, evt.target.value, null, null);
+
+	FormsProcessor.dispatchEvent(pThis.element,oEvt);
+	/*
+	 * Cancel bubbling but don't cancel the event itself
+	 * otherwise we never get the value actually changed.
+	 */
+
+	 evt.cancelBubble = true;
+
+}
+
+XFormsInputValue.prototype.getOwnerNodeName  = function()
+{
+	var sNodeName = this.element.parentNode.nodeName;
+	return sNodeName.slice(sNodeName.indexOf(":")+1,sNodeName.length).toLowerCase();
+}
+
+XFormsInputValue.prototype.onDocumentReady = function()
+{
+	if (this.element.ownerDocument.media != "print")
+	{
+		var sTagNameLC = this.getOwnerNodeName();
+		var sElementToCreate = (sTagNameLC == "textarea")?"textarea":"input"; 
+		var oInput = document.createElement(sElementToCreate);
+	
+		oInput.style.backgroundColor = "transparent";
+		oInput.style.padding = "0";
+		oInput.style.margin = "0";
+		oInput.style.border = "0";
+
+		var pThis = this;
+		if(document.all)
+		{
+			oInput.attachEvent("onchange", function(e){valueChangedIE(pThis,e);});
+		}
+		else
+		{
+			oInput.addEventListener("change",function(e){valueChangedFF(pThis,e);},false);
+		}
+
+			
+		if(sTagNameLC== "secret")
+		{
+			oInput.type="password";
+		}
+		else if(sTagNameLC !== "textarea")
+		{
+			oInput.setAttribute("type","text");
+		}
+			
+		this.element.appendChild(oInput);
+
+			/*
+			* [ISSUE] Stick with other method of always
+			* 'locating' things just when we need them?
+			*/
+
+		this.m_value = oInput;
+		//this.m_value.value = "null value";		
+
+	}
+};
+
+XFormsInputValue.prototype.setValue = function(sValue)
+{
+	var bRet = false;
+
+	if (this.m_value.value != sValue)
+	{
+		this.m_value.value = sValue;
+		this.currValue = sValue;
+		bRet = true;
+	}
+	else if(m_bFirstSetValue)
+	{
+		bRet = true;
+		m_bFirstSetValue = false;
+	}
+	
+	return bRet;
+};
