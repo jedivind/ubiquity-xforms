750bc52d042bfe859d907a180befd1ab182e3799
wellsk0705 <wellsk0705@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-08-12 20:09:37 +0000

    Merged trunk changes r512:597 into 'issue-30-submission problems'.

--- branches/issue-30-submission problems/lib/xforms/xforms-loader.js
+++ branches/issue-30-submission problems/lib/xforms/xforms-loader.js
@@ -60,6 +60,10 @@ var g_sBehaviourDirectory  = "";
   	loader.addModule({ name: "libxh-decorator",            type: "js",  fullpath: moduleBase + "../decorate.js", 
   		requires:["libxh-namespace-manager"]});
   
+    // crypto
+    loader.addModule({ name: "xpath-extension-md5",          type: "js",  fullpath: moduleBase + "../third-party/md5.js" });
+    loader.addModule({ name: "xpath-extension-sha1",        type: "js",  fullpath: moduleBase + "../third-party/sha1.js" });
+
   	loader.addModule({ name: "xforms-dom-misc",            type: "js",  fullpath: moduleBase + "../ajaxslt/misc.js" });
   	loader.addModule({ name: "xforms-dom",                 type: "js",  fullpath: moduleBase + "../ajaxslt/dom.js",
   		requires: [ "xforms-dom-misc" ] });
@@ -67,8 +71,9 @@ var g_sBehaviourDirectory  = "";
   	loader.addModule({ name: "xforms-ajaxslt-improvements", type: "js",  fullpath: moduleBase + "ajaxslt-improvements.js",
   		requires: [ "xforms-dom", "xforms-xpath" ] });
   	loader.addModule({ name: "xforms-core-function-library", type: "js",  fullpath: moduleBase + "xforms-core-function-library.js",
-  	requires: [ "xforms-xpath" ] });
-  	
+  	requires: [ "xforms-xpath", "xpath-extension-md5", "xpath-extension-sha1" ] });
+
+
   	loader.addModule({ name: "xforms-instance",            type: "js",  fullpath: moduleBase + "Instance.js",
   		requires: ["xforms-dom", "xforms-dom2events", "xforms-ajaxslt-improvements", "xforms-core-function-library" ] });
   	
