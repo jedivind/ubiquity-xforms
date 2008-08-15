ccdd0c1897c0721bfd65a8de094fbec57bb4397b
mdmcente <mdmcente@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-08-15 14:57:03 +0000

    Fixed the alert test case and added some comments to Alert.js. This revision does NOT need a code review.

--- branches/implement-alert/lib/xforms/Alert.js
+++ branches/implement-alert/lib/xforms/Alert.js
@@ -20,24 +20,29 @@ function Alert(elmnt)
 
 	if(this.element.localName.toLowerCase() == "xf:alert")
 	{
+		//  This allows the new alert element to be valid or invalid.
+		var reference = "";
+		if(this.element.parentNode.getAttribute("ref")) {
+			reference = this.element.parentNode.getAttribute("ref");
+		}
+		else {
+			reference = this.element.parentNode.getAttribute("value");
+		}
+		//  A new alert element is added after the control, because I couldn't move xf:alert
+		//  to a good spot and I couldn't create another xf:alert in a good spot.
+		//  In other words, making <alert> works and making <xf:alert> doesn't.
+		//  Counterintuitively, insertAdjacentHTML works in Firefox, and createElement in IE.
+		//	If createElement is used in firefox, the xbl does not bind.
 		if(document.all)
 		{
 			var newAlert = document.createElement("alert");
-			newAlert.setAttribute("id", "newAlert");
+			newAlert.setAttribute("ref", reference);
 			var alert_content = document.createTextNode(this.element.textContent);
 			newAlert.appendChild(alert_content);
 			this.element.parentNode.insertBefore(newAlert, this.element.nextSibling);
 		}
 		else
 		{
-			var al = this.element;
-			var reference = "";
-			if(this.element.parentNode.getAttribute("ref")) {
-				reference = this.element.parentNode.getAttribute("ref");
-			}
-			else {
-				reference = this.element.parentNode.getAttribute("value");
-			}
 			var alert_html = "<alert ref='" + reference + "'>" + this.element.innerHTML + "</alert>";
 			this.element.parentNode.insertAdjacentHTML("afterEnd",alert_html);
 		}
