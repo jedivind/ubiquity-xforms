4c5498f64f40f5f3a425b4359ad55a777676bdd8
mark.birbeck <mark.birbeck@webBackplane.com>
2008-10-04 11:36:37 +0000

    Added initial code and unit tests for a deleteNodes() method on the Instance object.

--- _unit-tests/unit-test-loader.js
+++ _unit-tests/unit-test-loader.js
@@ -42,6 +42,9 @@ function runTheTests() {
   loader.addModule({ name: "ux-ut-instance-standalone", type: "js",  fullpath: moduleBase + "ut-instance-standalone.js",
     requires: [ "yuitest", "logger-css", "test-logger-css" ] });
 
+  loader.addModule({ name: "ux-ut-delete-nodes", type: "js",  fullpath: moduleBase + "ut-delete-nodes.js",
+    requires: [ "yuitest", "logger-css", "test-logger-css" ] });
+
   loader.addModule({ name: "ux-ut-model-standalone", type: "js",  fullpath: moduleBase + "ut-model-standalone.js",
     requires: [ "yuitest", "logger-css", "test-logger-css" ] });
 
@@ -54,10 +57,8 @@ function runTheTests() {
   loader.addModule({ name: "ux-ut-finite-control", type: "js",  fullpath: moduleBase + "ut-finite-control.js",
     requires: [ "yuitest", "logger-css", "test-logger-css" ] });
 
-  
-  
   loader.require( "ux-ut-xforms-library-loaded", "ux-ut-xpath-core-functions", "ux-ut-NamespaceManager", "ux-ut-path-to-module", "ux-ut-reset",
-     "ux-ut-model-standalone","ux-ut-instance-standalone", "ux-ut-select1", "ux-ut-finite-control");
+     "ux-ut-model-standalone","ux-ut-instance-standalone", "ux-ut-select1", "ux-ut-finite-control", "ux-ut-delete-nodes");
   
   var sBars = "";
   loader.onProgress = function(o) {
@@ -70,7 +71,8 @@ function runTheTests() {
     var logger = new YAHOO.tool.TestLogger();
     window.status = "testing"; 
 
-    //add the test suite to the runner's queue
+    // Add the test suite to the runner's queue.
+    //
     YAHOO.tool.TestRunner.add(oSuitePathToModule);
     YAHOO.tool.TestRunner.add(suiteXFormsLibraryLoaded);
     YAHOO.tool.TestRunner.add(suiteXPathCoreFunctions);
@@ -81,11 +83,13 @@ function runTheTests() {
 
     YAHOO.tool.TestRunner.add(suiteFiniteControl);
     YAHOO.tool.TestRunner.add(suiteSelect1);
-    //run the tests
+
+    // Run the tests.
+    //
     YAHOO.tool.TestRunner.run();
     window.status = "tested"; 
-
-  };
+		return;
+  };// onSucess()
   
   loader.insert();
   return;
