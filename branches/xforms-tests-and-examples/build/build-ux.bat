9e449628e28de19cb7f88e25568c942be7a7e0ee
mdmcente <mdmcente@702bd106-314c-0410-bf92-7b4dcd54591c>
2008-12-10 15:50:30 +0000

    Merged trunk into the xforms-tests-and-examples branch and started updating the test cases with 'src'.

--- branches/xforms-tests-and-examples/build/build-ux.bat
+++ branches/xforms-tests-and-examples/build/build-ux.bat
@@ -5,5 +5,5 @@ REM
 copy %~dp0\..\package\ubiquity-xforms.js %~dp0\..\package\ubiquity-xforms.js.bak
 copy %~dp0\..\package\ubiquity-xforms.css %~dp0\..\package\ubiquity-xforms.css.bak
 
-cscript %~dp0\rollup\make-rollup.wsf /js:%~dp0\..\package\ubiquity-xforms.js /css:%~dp0\..\assets\ubiquity-xforms.css /paths:xforms-loader:file:///%~dp0../lib/xforms/, < %~dp0\..\lib\xforms\xforms-loader.js
+cscript %~dp0\rollup\make-rollup.wsf /js:%~dp0\..\package\ubiquity-xforms.js /css:%~dp0\..\src\assets\ubiquity-xforms.css /paths:xforms-loader:file:///%~dp0../src/lib/xforms/, < %~dp0\..\src\lib\xforms\xforms-loader.js
 
