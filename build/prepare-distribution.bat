309635fa4e0b0bc18b0dfc061f55c298e259a367
michael.junkin <michael.junkin@gmail.com>
2009-01-12 22:37:55 +0000

    1627 - Modified deploy scripts to permit deployment to any location.

--- build/prepare-distribution.bat
+++ build/prepare-distribution.bat
@@ -1,5 +1,4 @@
-REM Chains the operations needed for creating a distribution, 
-REM   pending QA and commission
+REM Chains the operations needed for creating a distribution, pending QA and commission
 CALL create-dist-directory.bat
 CALL build-ux.bat
 CALL copy-distributables.bat
