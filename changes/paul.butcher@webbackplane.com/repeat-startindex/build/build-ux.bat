cbd54ab78308a5725c79ad44fad1f46f9ace9436
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-01-13 12:36:24 +0000

    Deleted redundant change folders.

--- changes/paul.butcher@webbackplane.com/repeat-startindex/build/build-ux.bat
+++
@@ -1,9 +0,0 @@
-REM Build script for creating rolled up versions of ubiquity-xforms.
-REM 
-
-
-copy %~dp0\..\package\ubiquity-xforms.js %~dp0\..\package\ubiquity-xforms.js.bak
-copy %~dp0\..\package\ubiquity-xforms.css %~dp0\..\package\ubiquity-xforms.css.bak
-
-cscript %~dp0\rollup\make-rollup.wsf /js:%~dp0\..\package\ubiquity-xforms.js /css:%~dp0\..\src\assets\ubiquity-xforms.css /paths:xforms-loader:file:///%~dp0../src/lib/xforms/, < %~dp0\..\src\lib\xforms\xforms-loader.js
-
