4fa9c866008c2d8c5b3b1d3e9b38e50559a44675
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-01-28 09:47:29 +0000

    created setindex for review

---
+++ changes/paul.butcher@webbackplane.com/setindex/src/lib/xforms/range-value.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2008 Backplane Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*global spawn, FormsProcessor, YAHOO*/
+/*members Slider, createEvent, currValue, dispatchEvent, element, 
+    getHorizSlider, getValue, initEvent, initMutationEvent, innerHTML, 
+    m_bFirstSetValue, m_value, media, onDocumentReady, ownerDocument, 
+    prototype, setValue, subscribe, widget
+*/
+
+var rangecount = 0;
+
+function RangeValue(elmnt)
+{
+	this.element = elmnt;
+	this.currValue = "";
+	this.m_bFirstSetValue = true;
+}
+
+
+function rangeValueChanged(pThis, sNewValue)
+{
+	var oEvt = pThis.element.ownerDocument.createEvent("MutationEvents");
+	if (oEvt.initMutationEvent === undefined) {
+		oEvt.initMutationEvent = oEvt.initEvent;
+	}
+		
+	oEvt.initMutationEvent("control-value-changed", true, true,
+		null, pThis.currValue, sNewValue, null, null);
+
+	spawn(function () {
+			FormsProcessor.dispatchEvent(pThis.element, oEvt);
+	  });
+}
+
+RangeValue.prototype.onDocumentReady = function ()
+{
+	if (this.element.ownerDocument.media !== "print")
+	{
+		this.element.innerHTML = "<div id='slider-bg" + rangecount + "' class='slider-bg'><div class='slider-thumb' id='slider-thumb" + rangecount + "'> </div></div>";
+		this.m_value = YAHOO.widget.Slider.getHorizSlider("slider-bg" + rangecount, "slider-thumb" + rangecount, 0, 200, 20);
+		rangecount++;
+		
+		var pThis = this;
+		this.m_value.subscribe(
+			"slideEnd",
+			function () {
+    		rangeValueChanged(pThis, pThis.m_value.getValue() / 20);
+      }
+   );
+	}
+};
+
+RangeValue.prototype.setValue = function (sValue)
+{
+	var bRet = false;
+
+	if (this.m_value.getValue() !== (sValue * 20))
+	{
+		this.m_value.setValue(sValue * 20, true, true, true);
+		this.currValue = sValue;
+		bRet = true;
+	}
+	else if (this.m_bFirstSetValue)
+	{
+		bRet = true;
+		this.m_bFirstSetValue = false;
+	}
+	
+	return bRet;
+};
+
