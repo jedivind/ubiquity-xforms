bd23ccdd0da65902892d3e8f50a3965dac58f178
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-02-25 09:59:33 +0000

    Changed to bring in line with coding standard.

--- src/lib/xforms/conditional-invocation.js
+++ src/lib/xforms/conditional-invocation.js
@@ -14,8 +14,7 @@
  * limitations under the License.
  */
 
-ActionExecutor = function()
-{
+var ActionExecutor = function () {
 	/**
 		Evaluates an XPath expression and returns the result as a boolean
 		@param sCondition {String} The XPath expression to evaluate. 
@@ -23,15 +22,13 @@ ActionExecutor = function()
 		@returns true if the expression resulted in a boolean within oContext, false otherwise.  
 			Note that this means that if context is null, the return value will be false.  
 	*/
-	function evaluateCondition(sCondition,oContext)
-	{
-		var bIf = false;
-		if (oContext)
-		{
-			var oRes = oContext.model.EvaluateXPath(sCondition, oContext);
+	function evaluateCondition(sCondition, oContext)	{
+		var oRes, bIf;
+		bIf = false;
+		if (oContext) {
+			oRes = oContext.model.EvaluateXPath(sCondition, oContext);
 	
-			if (oRes)
-			{
+			if (oRes) {
 				bIf = oRes.booleanValue();
 			}
 		}
@@ -44,85 +41,66 @@ ActionExecutor = function()
 	 If there is no "if" property, this will evaluate to true.
 	@returns false if the the if condition evaluated to false, true otherwise
 	*/
-	function evaluateIfCondition(oListener,oContext)
-	{
+	function evaluateIfCondition(oListener, oContext) {
 		var bIf = true;
-		if (oListener.getAttribute("if"))
-		{
-			bIf = evaluateCondition(oListener.getAttribute("if"),oContext);
+		if (oListener.getAttribute("if")) {
+			bIf = evaluateCondition(oListener.getAttribute("if"), oContext);
 		}
 		return bIf;
 	}
 	
-	var itself = function()
-	{
+	var itself = { };
 	
-	};
-	
-	itself.invokeListener = function(oListener, oEvt)
-	{
+	itself.invokeListener = function (oListener, oEvt) {
+		var oRealListener, oContext, oRes, i;
 		//This is likely to have been called with a proxy listener, as defined in ConditionalInvocationListener
-		var oRealListener = oListener.realListener?oListener.realListener:oListener;
+		oRealListener = oListener.realListener?oListener.realListener:oListener;
 		//oRealListener will be the actual handler for the event that may contain the parameters for conditional invocation
 		//oListener will be the object on which to call handleEvent, it will pass the call on to the real listener.
 		
-		if(typeof oRealListener.getEvaluationContext != "undefined")
-		{
-			var oContext = oRealListener.getEvaluationContext();
-			if(oRealListener.getAttribute("iterate"))
-			{
-				var oRes = oContext.model.EvaluateXPath(oRealListener.getAttribute("iterate"), oContext);
-				if(oRes && oRes.value)
-				{
-					for(var i = 0; i < oRes.value.length; ++i)
-					{
+		if (typeof oRealListener.getEvaluationContext !== "undefined") {
+			oContext = oRealListener.getEvaluationContext();
+			if (oRealListener.getAttribute("iterate")) {
+				oRes = oContext.model.EvaluateXPath(oRealListener.getAttribute("iterate"), oContext);
+				if (oRes && oRes.value) {
+					for (i = 0; i < oRes.value.length; ++i) {
 						oRealListener.unwire();
 						oRealListener.m_context = {
-						    model:oContext.model,
-						    node:oRes.value[i],
+						    model: oContext.model,
+						    node: oRes.value[i],
 						    resolverElement: oRealListener.element,
 						    position: i,
 						    size: oRes.value.length
 						};
-						if (evaluateIfCondition(oRealListener,oRealListener.m_context))
-						{
-							if(oRealListener.getAttribute("while"))
-							{
-								while(evaluateCondition(oRealListener.getAttribute("while"),oRealListener.m_context))
-								{
+						
+						if (evaluateIfCondition(oRealListener, oRealListener.m_context)) {
+							if (oRealListener.getAttribute("while")) {
+								while (evaluateCondition(oRealListener.getAttribute("while"), oRealListener.m_context)) {
 									oListener.handleEvent(oEvt);
 								}
-							}
-							else
-							{
+							} else {
 								oListener.handleEvent(oEvt);
 							}
 						}
 					}
 				}
 					
-			}
-			else if (evaluateIfCondition(oRealListener,oContext))
-			{	
-				if(oRealListener.getAttribute("while"))
-				{
-					while(evaluateCondition(oRealListener.getAttribute("while"),oContext))
-					{
+			} else if (evaluateIfCondition(oRealListener, oContext)) {	
+				if (oRealListener.getAttribute("while")) {
+					while (evaluateCondition(oRealListener.getAttribute("while"), oContext)) {
 						oListener.handleEvent(oEvt);
 						oContext = oRealListener.getEvaluationContext();
 					}
 				}
-				else
-				{
+				else {
 					oListener.handleEvent(oEvt);
 				}
 			}
 		}
-		else
-		{
+		else {
 			oListener.handleEvent(oEvt);
 		}
-	    return;
+		return;
 	};
 	return itself;
 }();
