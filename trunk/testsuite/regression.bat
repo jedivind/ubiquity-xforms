d4999a70808c4653e45e59896d6210f7b99f9c3f
mark.birbeck@webBackplane.com <mark.birbeck@webBackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-04 04:23:36 +0000

    1. Added build.xml to use Ant to check whether the regression tests passed or failed. 2. Added the Ant check to the regression testing batch file.

--- trunk/testsuite/regression.bat
+++ trunk/testsuite/regression.bat
@@ -1,1 +1,2 @@
-C:/Docume~1/buildbot/full/build/testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=regressionResults.html&close=true"
+C:/Docume~1/buildbot/full/build/testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regressionResults.html&close=true"
+ant
