d396254daeadbb1f61194753823c2aa9548b50d8
mark.birbeck <mark.birbeck@webBackplane.com>
2009-03-04 05:10:03 +0000

    Reinstated the Ant check after running Selenium, but we now generate an exit code.

--- testsuite/regression.bat
+++ testsuite/regression.bat
@@ -1,1 +1,3 @@
 C:/Docume~1/buildbot/full/build/testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regressionResults.html&close=true"
+ant -f "testsuite/build.xml"
+if "%ERRORLEVEL%" == "1" exit /b 1
