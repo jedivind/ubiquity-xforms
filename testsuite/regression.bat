7bba1a1aaf159426295af264749090cf34c8ec82
mark.birbeck <mark.birbeck@webBackplane.com>
2009-03-04 04:41:41 +0000

    1. Fixed typo on directory for Ant command. 2. Made path to the .hta file relative.

--- testsuite/regression.bat
+++ testsuite/regression.bat
@@ -1,2 +1,2 @@
-C:/Docume~1/buildbot/full/build/testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regressionResults.html&close=true"
-ant -f testsuit/build.xml
+testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regressionResults.html&close=true"
+ant -f testsuite/build.xml
