d5aeec1e7b8a5f00f33119a71b2570f760d2b3a9
phil.booth@webbackplane.com <phil.booth@webbackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-05 15:10:37 +0000

    Deleted changes branch.

--- changes/phil.booth@webbackplane.com/issues-295-and-296/testsuite/xforms11-ie7.bat
+++
@@ -1,3 +0,0 @@
-"./testsuite/selenium/core/TestRunner.hta" "test=../../W3C-XForms-1.1/Edition1/driverPages/SeleniumTests/W3CTestSuite.html&auto=true&save=true&resultsUrl=testsuite/xforms11-ie7-results.html&close=true"
-ant -f "testsuite/build.xml"
-if "%ERRORLEVEL%" == "1" exit 1
