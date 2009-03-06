c6c8f32487f79f1859816ade7a292eac16166015
mark.birbeck@webBackplane.com <mark.birbeck@webBackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-06 00:32:30 +0000

    Removed final line that tested the errorlevel, in each of the testing batch files. The problem this was attempting to resolve has been fixed (and it didn't work anyway).

--- trunk/testsuite/regression-ie7.bat
+++ trunk/testsuite/regression-ie7.bat
@@ -1,3 +1,2 @@
 "./testsuite/selenium/core/TestRunner.hta" "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regression-ie7-results.html&close=true"
 ant -f "testsuite/build.xml"
-if "%ERRORLEVEL%" == "1" exit 1
