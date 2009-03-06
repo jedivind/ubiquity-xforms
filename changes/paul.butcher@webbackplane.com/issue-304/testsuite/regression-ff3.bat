7e8f8ae7c69ebd658bcdf23209272dc8f748fb1f
paul.butcher <paul.butcher@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-06 15:06:52 +0000

    deleting erroneous change branch

--- changes/paul.butcher@webbackplane.com/issue-304/testsuite/regression-ff3.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Mozilla Firefox\firefox.exe" "testsuite\selenium\core\TestRunner.html?test=..\..\issues\driverPages\SeleniumTests\TestSuite.html&auto=true&save=true&resultsUrl=..\..\regression-ff3-results.html&close=true"
-ant -f "testsuite\build.xml"
-if "%ERRORLEVEL%" == "1" exit 1
