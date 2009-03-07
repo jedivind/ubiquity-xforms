6469f99cefccfb69e6f81ce25e1fdd4f5fe6617d
moishe.t <moishe.t@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-07 03:40:19 +0000

    deleting review copy

--- changes/moishe.t/deleteNodes/testsuite/regression.bat
+++
@@ -1,1 +0,0 @@
-C:/Docume~1/buildbot/full/build/testsuite/selenium/core/TestRunner.hta "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=regressionResults.html&close=true"
