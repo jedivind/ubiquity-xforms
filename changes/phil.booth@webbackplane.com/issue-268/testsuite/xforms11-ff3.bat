c1d1cb1f1e100595a3441c123661d2dd1f004f47
phil.booth@webbackplane.com <phil.booth@webbackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-08 19:23:43 +0000

    Proposed change to fix issue 268 ("controls within non-selected cases should be disabled, and despatch events accordingly").

---
+++ changes/phil.booth@webbackplane.com/issue-268/testsuite/xforms11-ff3.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Mozilla Firefox\firefox.exe" "testsuite\selenium\core\TestRunner.html?test=..\..\W3C-XForms-1.1\Edition1\driverPages\SeleniumTests\W3CTestSuite.html&auto=true&save=true&resultsUrl=..\..\xforms11-ff3-results.html&close=true"
+ant -f "testsuite\build.xml"
