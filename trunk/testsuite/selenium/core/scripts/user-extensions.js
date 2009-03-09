51bd338e0e44035a547b815cd269f3e0150d70d3
mark.birbeck@webBackplane.com <mark.birbeck@webBackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-09 22:58:09 +0000

    Upgraded Selenium from 0.8.0 to 0.8.2. Note that the only impact this _should_ have on the tests is the change to the way that relative paths work. There are some notes on this in the 'testsuite/index.html' file. The 'issues' regression tests have had their paths modified so that I had something to test with, but I haven't touched the others yet.

--- trunk/testsuite/selenium/core/scripts/user-extensions.js
+++ trunk/testsuite/selenium/core/scripts/user-extensions.js
@@ -114,3 +114,112 @@ Selenium.prototype.assertXFormsProperty = function(locator,propObjString)
 	}
 }
 
+
+Selenium.prototype.findEffectiveStyleProperty = function(element, property) {
+	var propertyValue = "";
+	if (selenium.browserbot.getCurrentWindow().getComputedStyle){
+		propertyValue = selenium.browserbot.getCurrentWindow().getComputedStyle(element, null).getPropertyValue(property);
+	}
+	else if (element.currentStyle){
+		property = property.replace(/\-(\w)/g, function (strMatch, p1){
+			return p1.toUpperCase();
+		});
+		propertyValue = element.currentStyle[property];
+	}
+	return propertyValue;
+}
+
+TestResult.prototype.getLocalPath = function(origPath) {
+	var originalPath = /* convertUriToUTF8( */ origPath /*,config.options.txtFileSystemCharSet)*/;
+	// Remove any location or query part of the URL
+	var argPos = originalPath.indexOf("?");
+	if (argPos != -1)
+		originalPath = originalPath.substr(0,argPos);
+	var hashPos = originalPath.indexOf("#");
+	if (hashPos != -1)
+		originalPath = originalPath.substr(0,hashPos);
+	// Convert file://localhost/ to file:///
+	if(originalPath.indexOf("file://localhost/") == 0)
+		originalPath = "file://" + originalPath.substr(16);
+	// Convert to a native file format
+	var localPath;
+	if(originalPath.charAt(9) == ":") // pc local file
+		localPath = unescape(originalPath.substr(8)).replace(new RegExp("/","g"),"\\");
+	else if(originalPath.indexOf("file://///") == 0) // FireFox pc network file
+		localPath = "\\\\" + unescape(originalPath.substr(10)).replace(new RegExp("/","g"),"\\");
+	else if(originalPath.indexOf("file:///") == 0) // mac/unix local file
+		localPath = unescape(originalPath.substr(7));
+	else if(originalPath.indexOf("file:/") == 0) // mac/unix local file
+		localPath = unescape(originalPath.substr(5));
+	else // pc network file
+		localPath = "\\\\" + unescape(originalPath.substr(7)).replace(new RegExp("/","g"),"\\");
+	return localPath;
+};
+
+
+TestResult.prototype._saveToFile = function (fileName, form) {
+        // This works when run as an IE HTA and in FF
+        var inputs = new Object();
+        for (var i = 0; i < form.elements.length; i++) {
+            inputs[form.elements[i].name] = form.elements[i].value;
+        }
+
+        var objFSO, scriptFile;
+
+        if (navigator.userAgent.toLowerCase().indexOf("gecko") !== -1) {
+						fileName = this.getLocalPath(
+							makeAbsoluteURI(document.location.toString(), fileName)
+						);
+            netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+						var file = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+						
+						//try {
+							file.initWithPath(fileName);
+						//} catch(e) {
+						//	throw "Bad file name";
+						//}
+						//try {
+							if (!file.exists()) {
+								file.create(Components.interfaces.nsIFile.NORMAL_FILE_TYPE, 420);
+							}
+						//} catch(e) {
+						//	throw "Cannot create file";
+						//}
+            scriptFile = {
+            		stream: Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream),
+
+								WriteLine: function(s) {
+									return this.stream.write(s, s.length);
+								},
+
+								Close: function() {
+									return this.stream.close();
+								}
+						};
+						//try {
+							scriptFile.stream.init(file, 0x02 | 0x08 | 0x20, 0666, 0);
+						//} catch(e) {
+						//	throw "Cannot create file";
+						//}
+        } else {
+            objFSO = new ActiveXObject("Scripting.FileSystemObject");
+            scriptFile = objFSO.CreateTextFile(fileName);
+				}
+
+         scriptFile.WriteLine("<html><body>\n<h1>Test suite results </h1>" +
+                             "\n\n<table>\n<tr>\n<td>result:</td>\n<td>" + inputs["result"] + "</td>\n" +
+                             "</tr>\n<tr>\n<td>totalTime:</td>\n<td>" + inputs["totalTime"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>numTestPasses:</td>\n<td>" + inputs["numTestPasses"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>numTestFailures:</td>\n<td>" + inputs["numTestFailures"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>numCommandPasses:</td>\n<td>" + inputs["numCommandPasses"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>numCommandFailures:</td>\n<td>" + inputs["numCommandFailures"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>numCommandErrors:</td>\n<td>" + inputs["numCommandErrors"] + "</td>\n</tr>\n" +
+                             "<tr>\n<td>" + inputs["suite"] + "</td>\n<td>&nbsp;</td>\n</tr>");
+        var testNum = inputs["numTestTotal"];
+        for (var rowNum = 1; rowNum < testNum; rowNum++) {
+            scriptFile.WriteLine("<tr>\n<td>" + inputs["testTable." + rowNum] + "</td>\n<td>&nbsp;</td>\n</tr>");
+        }
+        scriptFile.WriteLine("</table></body></html>");
+        scriptFile.Close();
+    }
+
