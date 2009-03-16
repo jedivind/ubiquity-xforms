ba857cce16159e1335c9bcb5f89f0ca568b31471
boyerj@ca.ibm.com <boyerj@ca.ibm.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-03-16 00:58:30 +0000

    Delete after commit of r2421 to trunk

--- changes/john.boyer/validation-fixes/testsuite/regression-ie7.bat
+++
@@ -1,2 +0,0 @@
-"./testsuite/selenium/core/TestRunner.hta" "test=../../issues/driverPages/SeleniumTests/TestSuite.html&auto=true&save=true&resultsUrl=testsuite/regression-ie7-results.html&close=true"
-ant -f "testsuite/build.xml"
