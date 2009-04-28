0e5494b1ecd09ad0c3d0552e10a5ab1a943346cd
bryan.kyle <bryan.kyle@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-04-28 21:37:21 +0000

    Creating change directory for issue-406

---
+++ changes/bryan.kyle/issue-406/build/build-ux.bat
@@ -0,0 +1,13 @@
+@REM Build script for creating rolled up versions of ubiquity-xforms.
+
+@if not "%1"=="no-compress" set compress-arg= & goto do-build
+@set compress-arg="/noCompression"
+:do-build
+
+@if not exist %~dp0\..\package mkdir %~dp0\..\package
+
+@if exist %~dp0..\package\ubiquity-xforms.js copy /Y %~dp0..\package\ubiquity-xforms.js %~dp0\..\package\ubiquity-xforms.js.bak
+@if exist %~dp0..\package\ubiquity-xforms.css copy /Y %~dp0..\package\ubiquity-xforms.css %~dp0\..\package\ubiquity-xforms.css.bak
+
+cscript %~dp0\rollup\make-rollup.wsf %compress-arg% /js:%~dp0\..\package\ubiquity-xforms.js /css:%~dp0\..\src\assets\ubiquity-xforms.css /paths:xforms-loader:file:///%~dp0../src/lib/xforms/ < %~dp0\..\src\lib\xforms\xforms-loader.js
+
