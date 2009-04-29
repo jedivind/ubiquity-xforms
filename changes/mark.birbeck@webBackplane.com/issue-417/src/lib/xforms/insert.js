0f7d7fa5e19703c308bcd8e4d004c0b8e530fc2e
markbirbeck <markbirbeck@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-04-29 11:17:13 +0000

    Creating 'issue-417' for review of proposed changes for issue 417 ('Current index of a nested repeat is not being maintained when the repeat moves position').

---
+++ changes/mark.birbeck@webBackplane.com/issue-417/src/lib/xforms/insert.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2008-2009 Backplane Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+function Insert(elmnt)
+{
+	this.element = elmnt;
+}
+
+Insert.prototype.handleEvent = DeferToConditionalInvocationProcessor;
+
+Insert.prototype.performAction = function (evt)
+{
+	var oContext = this.getEvaluationContext(),
+		bindid = this.element.getAttribute("bind"),
+		atExpr = this.element.getAttribute("at"),
+		positionExpr = this.element.getAttribute("position"),
+		originExpr = this.element.getAttribute("origin"),
+		oInstance = oContext.model.instances()[0],
+		nodesetExpr,
+		nodeset,
+		bindObject;
+
+	if (bindid) {
+		bindObject = FormsProcessor.getBindObject(bindid, this.element);
+		nodeset = bindObject.boundNodeSet;
+	} else {
+		nodesetExpr = this.element.getAttribute("nodeset");
+		nodeset = (nodesetExpr) ? oInstance.evalXPath(nodesetExpr, oContext).nodeSetValue() : null;
+	}
+
+	if (oInstance.insertNodeset(oContext, nodeset, atExpr, positionExpr, originExpr)) {
+		oContext.model.flagRebuild();
+	}
+	this.m_context = null;
+};
