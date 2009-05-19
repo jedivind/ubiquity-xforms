3c321bc8489c503e3b9adb9637ce09227a398718
phil.booth <phil.booth@webbackplane.com>
2009-05-19 10:09:13 +0000

    Modified one of the rollup scripts to make it BuildBot-friendly.

--- build/create-dist-directory.bat
+++ build/create-dist-directory.bat
@@ -1,13 +1,11 @@
-REM Creates a dist directory on the server, 
-REM 	then creates a Working copy and populates it with the current test material
+REM Creates a dist directory and populates it with the current test material
 
+rmdir /s %~dp0dist /q
+mkdir %~dp0dist
+mkdir %~dp0dist\samples
+mkdir %~dp0dist\testsuite
+mkdir %~dp0dist\unit-tests
 
-SET /P DIST_NUMBER=What is the identifier for this distribution (e.g. 0.6.2)
-rmdir /s %~dp0\dist /q
-mkdir %~dp0\dist
-
-REM svn mkdir -m "Creating directory to contain distribution %DIST_NUMBER%" https://ubiquity-xforms.googlecode.com/svn/dist/%DIST_NUMBER%
-svn checkout https://ubiquity-xforms.googlecode.com/svn/dist/%DIST_NUMBER% %~dp0\dist
-svn copy %~dp0\..\samples %~dp0\dist\samples\
-svn copy %~dp0\..\testsuite %~dp0\dist\testsuite\
-svn copy %~dp0\..\unit-tests %~dp0\dist\unit-tests\
+xcopy /E /Q %~dp0..\samples %~dp0dist\samples
+xcopy /E /Q %~dp0..\testsuite %~dp0dist\testsuite
+xcopy /E /Q %~dp0..\unit-tests %~dp0dist\unit-tests
