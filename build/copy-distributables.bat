95781655cf44ec3b8f13cfc3bf2185198229ba79
phil.booth <phil.booth@webbackplane.com>
2009-05-19 14:26:44 +0000

    Fix for issue 451 ("Rollup scripts don't honour correct directory structure").

--- build/copy-distributables.bat
+++ build/copy-distributables.bat
@@ -1,19 +1,21 @@
-@REM Copies the rolled up build and other distributables into a waiting dist directory
+@ECHO OFF
 
-@setlocal
+REM Copies the rolled up build and other distributables into a waiting dist directory
 
-@set deploy=%1
-@if not "%deploy%"=="" goto do-deploy
-@set deploy=%~dp0\dist
-@:do-deploy
+setlocal
 
-@if not exist "%deploy%\lib\xforms" mkdir %deploy%\lib\xforms
+set deploy=%1
+if not "%deploy%"=="" goto do-deploy
+set deploy=%~dp0dist
+:do-deploy
 
-@xcopy /Y /F %~dp0..\package %deploy%\package /I
-@xcopy /Y /F %~dp0..\src\assets %deploy%\assets /I /S
-@xcopy /Y /F %~dp0..\src\behaviours %deploy%\behaviours /I
-@xcopy /Y /F %~dp0..\src\ubiquity-loader.js %deploy%
-@xcopy /Y /F %~dp0..\src\lib\sniffer.js %deploy%\lib
-@xcopy /Y /F %~dp0..\src\lib\xforms\ie-instance-fixer.js %deploy%\lib\xforms
-@xcopy /Y /F %~dp0..\src\lib\xforms\ie6-css-selectors-fixer.js %deploy%\lib\xforms
-@xcopy /Y /F %~dp0..\src\lib\xforms\set-document-loaded.js %deploy%\lib\xforms
+if not exist "%deploy%\src\lib\xforms" mkdir %deploy%\src\lib\xforms
+
+xcopy /Y /F %~dp0..\package %deploy%\src\package /I
+xcopy /Y /F %~dp0..\src\assets %deploy%\src\assets /I /S
+xcopy /Y /F %~dp0..\src\behaviours %deploy%\src\behaviours /I
+xcopy /Y /F %~dp0..\src\ubiquity-loader.js %deploy%\src
+xcopy /Y /F %~dp0..\src\lib\sniffer.js %deploy%\src\lib
+xcopy /Y /F %~dp0..\src\lib\xforms\ie-instance-fixer.js %deploy%\src\lib\xforms
+xcopy /Y /F %~dp0..\src\lib\xforms\ie6-css-selectors-fixer.js %deploy%\src\lib\xforms
+xcopy /Y /F %~dp0..\src\lib\xforms\set-document-loaded.js %deploy%\src\lib\xforms
