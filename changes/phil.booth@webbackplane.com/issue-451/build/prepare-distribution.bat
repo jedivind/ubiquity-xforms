8ba0433a651cf9f9172ee4aa556d4ee484eb3462
phil.booth@webbackplane.com <phil.booth@webbackplane.com@702bd106-314c-0410-bf92-7b4dcd54591c>
2009-05-19 14:31:12 +0000

    Deleted changes branch.

--- changes/phil.booth@webbackplane.com/issue-451/build/prepare-distribution.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-REM Chains the operations needed for creating a distribution, pending QA and commission
-
-CALL create-dist-directory.bat
-CALL build-ux.bat
-CALL copy-distributables.bat
