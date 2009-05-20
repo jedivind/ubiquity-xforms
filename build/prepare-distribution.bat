032dd0762fef7ae5654c0e2152ff32e59abc3909
phil.booth <phil.booth@webbackplane.com>
2009-05-20 09:38:51 +0000

    Set Windows ERRORLEVEL from the rollup scripts in case of failure.

--- build/prepare-distribution.bat
+++ build/prepare-distribution.bat
@@ -4,4 +4,7 @@ REM Chains the operations needed for creating a distribution, pending QA and com
 
 CALL create-dist-directory.bat
 CALL build-ux.bat
+
+IF %ERRORLEVEL% NEQ 0 EXIT /B 1
+
 CALL copy-distributables.bat
